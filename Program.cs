287552fc1d74d182c9c3c174c59addc7cb228e66
Jessica <jgonzales0688@gmail.com>
2023-02-04 20:32:43 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace SecondVariables_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jessica";
+            int age = 21;
+            char intial = 'J';
+            bool answer = false;
+            double num1 = 21.5;
+            decimal num2 = 22.5m;
+
+            Console.WriteLine($"Her name is {name}, and she is {age} years old" +
+                $" and its {answer} that she is {num1} but looks forward to turing {num2}");
+        }
+    }
+}
